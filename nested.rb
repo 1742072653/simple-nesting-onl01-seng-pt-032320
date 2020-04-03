5aa3788e0847c6f6ad6a0b6e068433d021f835f3
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 19:05:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,11 +57,11 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => "GUI",
+          :known_for => "G",
           :languages => ["C"]
         }
      }
